f9ce0e2a5c5e0fead1c46ce23f8de719b5915813
AvriliaKawonal14 <S22210355@student.unklab.ac.id>
2024-11-17 09:44:19 +0800

    ganti isi experience menjadi bahasa, lalu tambah firebase

--- src/components/experience/index.js
+++ src/components/experience/index.js
@@ -12,27 +12,13 @@ const Experience = () => {
     });
   }, []);
     return (
-        <section className="experience">
-        <h2>{Experience.title}</h2>
-        <div className="job">
-          <h3>PT Contoh Teknologi - Software Engineer</h3>
-          <span>2020 - Sekarang</span>
-          <p>
-            Membangun dan mengelola aplikasi web menggunakan JavaScript, HTML,
-            CSS, dan React. Memimpin tim kecil dalam proyek pengembangan
-            aplikasi khusus klien.
-          </p>
-        </div>
-        <div className="job">
-          <h3>XYZ Digital Solutions - Web Developer</h3>
-          <span>2018 - 2020</span>
-          <p>
-            Mengembangkan dan memelihara situs web e-commerce yang ramah
-            pengguna dan responsif. Bekerja sama dengan desainer dan tim
-            pemasaran untuk meningkatkan pengalaman pengguna.
-          </p>
-        </div>
-      </section>
+      <section className="skills">
+      <h2>{Experience.title}</h2>
+      <ul>
+        <li>{Experience.ingg}</li>
+        <li>{Experience.indo}</li>
+      </ul>
+    </section>
     )
 }
 
