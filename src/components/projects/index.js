cc919a1be28725e460601fe58b63fb10a2fc8f83
AvriliaKawonal14 <S22210355@student.unklab.ac.id>
2024-11-17 09:15:04 +0800

    add firebase skills

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -11,53 +11,51 @@ const Projects = () => {
       setProjects(data);
     });
   }, []);
-    return (
-      <section className="projects">
+  return (
+    <section className="projects">
       <h2>{projects.jdl}</h2>
       <div className="project">
-        <img 
-          src="images/dbms.png" 
-          alt="Sistem Monitoring Inventaris" 
-          className="project-image" 
+        <img
+          src="images/dbms.png"
+          alt="Sistem Monitoring Inventaris"
+          className="project-image"
         />
         <h3>{projects.tit1}</h3>
         <span>{projects.thntit1}</span>
         <p>{projects.ket1}</p>
       </div>
       <div className="project">
-        <img 
-          src="images/dbms.png" 
-          alt="Sistem Monitoring Inventaris" 
-          className="project-image" 
+        <img
+          src="images/dbms.png"
+          alt="Sistem Monitoring Inventaris"
+          className="project-image"
         />
         <h3>{projects.tit2}</h3>
         <span>{projects.thn2}</span>
         <p>{projects.ket2}</p>
       </div>
       <div className="project">
-        <img 
-          src="images/webdesign.png" 
-          alt="Aplikasi Manajemen Tugas" 
-          className="project-image" 
+        <img
+          src="images/webdesign.png"
+          alt="Aplikasi Manajemen Tugas"
+          className="project-image"
         />
         <h3>{projects.tit3}</h3>
         <span>{projects.thn3}</span>
         <p>{projects.ket3}</p>
       </div>
       <div className="project">
-        <img 
-          src="images/Robotic.jpg" 
-          alt="Sistem Monitoring Inventaris" 
-          className="project-image" 
+        <img
+          src="images/Robotic.jpg"
+          alt="Sistem Monitoring Inventaris"
+          className="project-image"
         />
         <h3>{projects.tit4}</h3>
         <span>{projects.thn4}</span>
         <p>{projects.ket4}</p>
       </div>
     </section>
-    
-    )
-}
+  );
+};
 
 export default Projects;
-
--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -1,17 +1,29 @@
-const skills = () => {
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
+
+const Skills = () => {
+  const [Skills, setSkills] = useState([]);
+  useEffect(() => {
+    const db = getDatabase();
+    const skillsRef = ref(db, "Skills");
+    onValue(skillsRef, (snapshot) => {
+      const data = snapshot.val();
+      setSkills(data);
+    });
+  }, []);
     return (
         <section className="skills">
-        <h2>Keahlian</h2>
+        <h2>{Skills.ttl}</h2>
         <ul>
-          <li>HTML, CSS, JavaScript, React, Phyton, C++, C#</li>
-          <li>Database: MySQL</li>
-          <li>Soft Skills: Manajemen Waktu, Problem Solving</li>
-          <li>Blender</li>
-          <li>Unity</li>
-          <li>Figma</li>
+          <li>{Skills.bhs}</li>
+          <li>{Skills.sft}</li>
+          <li>{Skills.figma}</li>
+          <li>{Skills.unt}</li>
+          <li>{Skills.blender}</li>
+          <li>{Skills.dbs}</li>
         </ul>
       </section>
     )
 }
 
-export default skills;
+export default Skills;
